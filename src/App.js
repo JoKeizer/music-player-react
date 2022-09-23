94ccc568fe4877993127158b5fa6030f3606bf63
Jo Bos <jobos@Jos-MacBook-Pro.local>
2022-09-23 13:45:15 +0200

    chillhop data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, {useState} from "react"
+
 import GlobalStyle from './components/GlobalStyle';
 
 
-//Adding Components
 import Player from './components/Player';
 import Song from './components/Song'
 
+import data from './data'
+
 function App() {
+console.log(data)
+const [songs, setsongs] = useState(data());
+
   return (
     <>
       <GlobalStyle />
---
+++ src/data.js
@@ -0,0 +1,87 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Green Valley Trail",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/09/b7f06cec8a0bef89429dd85f68bb718a31289ec6-300x300.jpg",
+      artist: "Ruck P",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41930",
+      color: ["#A67948", "#293731"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Highland Park After Dark",
+      cover:
+        "https://i.scdn.co/image/ab67616d0000b27365390185a80cc80ec325d4cbab67616d0000b27365390185a80cc80ec325d4cb",
+      artist: "Ruck P",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41931",
+      color: ["#A67948", "#293731"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
